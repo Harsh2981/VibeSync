4dd5b420593feaf35fd5b4e7cb6e546c1d556dc4
Harsh <abs0lutezero0169@gmail.com>
2026-05-22 10:13:28 +0530

    Refactor Firebase initialization code

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,20 @@
-// Firebase configuration and initialization for VibeSync
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCQv-JhBbmIk0wDCGV3m06LJkvektPDInk",
-  authDomain: "vibesync-7c103.firebaseapp.com",
-  projectId: "vibesync-7c103",
-  storageBucket: "vibesync-7c103.firebasestorage.app",
-  messagingSenderId: "274968218360",
-  appId: "1:274968218360:web:74bbbd8e82742e3442490d",
-  measurementId: "G-0WRYD3KLZZ"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-
-export default app;
+ 1 import { initializeApp } from "firebase/app";
+    2 import { getAuth } from "firebase/auth";
+    3 import { getFirestore } from "firebase/firestore";
+    4 import { getStorage } from "firebase/storage";
+    5
+    6 const firebaseConfig = {
+    7   apiKey: "AIzaSyCQv-JhBbmIk0wDCGV3m06LJkvektPDInk",
+    8   authDomain: "vibesync-7c103.firebaseapp.com",
+    9   projectId: "vibesync-7c103",
+   10   storageBucket: "vibesync-7c103.firebasestorage.app",
+   11   messagingSenderId: "274968218360",
+   12   appId: "1:274968218360:web:74bbbd8e82742e3442490d",
+   13   measurementId: "G-0WRYD3KLZZ"
+   14 };
+   15
+   16 const app = initializeApp(firebaseConfig);
+   17 export const auth = getAuth(app);
+   18 export const db = getFirestore(app);
+   19 export const storage = getStorage(app);
+   20 export default app;
